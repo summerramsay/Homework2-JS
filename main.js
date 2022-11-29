f8d7b23500cb523e28c87dd5b6f146146a716a21
SUMMERRAMSAY <summerdramsay@gmail.com>
2022-11-29 00:43:23 -0500

    Final commit

---
+++ main.js
@@ -0,0 +1,10 @@
+let firstName;
+let lastName;
+let age;
+firstName = "Summer"
+lastName = "Ramsay"
+age = 18
+alert("My name is " + firstName +" "+ lastName);
+alert("I am " + age + " years old")
+console.log("This class has been very interesting and a great learning experience.")
+
